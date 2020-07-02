c3b59e56c602135b449a04ccce241041051a84ec
Noé <noe.abdelkalek@gmail.com>
2020-07-02 19:54:26 +0200

    Fix Passenger Controller

--- src/symfony/src/Controller/PassengerController.php
+++ src/symfony/src/Controller/PassengerController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Passenger;
+use App\Repository\PassengerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,4 +13,45 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class PassengerController extends AbstractController
 {
+    /**
+     * @Route("/", name="passenger_index")
+     */
+    public function indexAction(PassengerRepository $repository)
+    {
+        $passengers = $repository->findAll();
+
+        return $this->render('passenger/index.html.twig', array(
+            'passengers' => $passengers
+        ));
+    }
+
+    /**
+     * @Route("/add", name="passenger_add")
+     */
+    public function addAction(Request $request)
+    {
+        $passenger = new Passenger();
+
+        $formulaire = $this->createFormBuilder($passenger)
+            ->add('firstName', TextType::class, ['label' => 'Prénom : '])
+            ->add('lastName', TextType::class, ['label' => 'Nom : '])
+            ->add('passportNumber', TextType::class, ['label' => 'Passeport : '])
+            ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $formulaire->handleRequest($request);
+
+            if ($formulaire->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($passenger);
+                $em->flush();
+                return $this->redirectToRoute('passenger_index');
+            }
+        }
+
+        return $this->render('passenger/add.html.twig', array(
+            'formulaire' => $formulaire->createView()
+        ));
+    }
 }
