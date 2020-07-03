fa7d0cbe618902912e71a08c3804062fc70d72f9
feinglong <feinglong@gmail.com>
2020-07-03 10:17:46 +0200

    ajout de la session

--- src/symfony/src/Controller/AdditionController.php
+++ src/symfony/src/Controller/AdditionController.php
@@ -4,25 +4,61 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 class AdditionController extends AbstractController
 {
     /**
      * @Route("/addition", name="addition")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
+
+        $xmin = $session->get('xmin', 1); 
+        $xmax = $session->get('xmax' , 5); 
+        $ymin = $session->get('ymin', 1); 
+        $ymax = $session->get('ymax', 5);
+        
+        $session->set('xmin', $xmin);
+        $session->set('xmax', $xmax);
+        $session->set('ymin', $ymin);
+        $session->set('ymax', $ymax);
+
+
         return $this->render('addition/index.html.twig', [
-            'controller_name' => 'AdditionController',
+            'controller_name' => 'IndexController',
+            'xmin' => $xmin,
+            'xmax' => $xmax,
+            'ymin' => $ymin,
+            'ymax' => $ymax,
         ]);
     }
 
      /**
      * @Route("/addition/{xmin}/{xmax}/{ymin}/{ymax}", name="addition_action", requirements = {"xmin"="^[1-9]\d*$","xmax"="^[1-9]\d*$","ymin"="^[1-9]\d*$","ymax"="^[1-9]\d*$", })
      */
-    public function additionAction($xmin, $xmax, $ymin, $ymax)
+    public function additionAction($xmin, $xmax, $ymin, $ymax, SessionInterface $session)
     {
         
+        if($xmin> $xmax) {
+            $tmp = $xmin;
+            $xmin = $xmax;
+            $xmax = $tmp; 
+        }
+
+        if($ymin> $ymax) {
+            $tmp = $ymin;
+            $ymin = $ymax;
+            $ymax = $tmp; 
+        }
+        
+        $session->set('xmin', $xmin);        
+        $session->set('xmax', $xmax);
+        $session->set('ymin', $ymin);
+        $session->set('ymax', $ymax);
+
+
         return $this->render('addition/addition.html.twig', [
             'controller_name' => 'AdditionController',
             'xmin' => $xmin,
